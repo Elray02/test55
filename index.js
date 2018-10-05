b7cb2a11e957a09ddad59777ad77a466c6ac934a
Ftasso <ray02@protonmail.com>
2018-10-05 11:59:35 +0200

    add day formatting

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ fetch("http://cdn.55labs.com/demo/api.json")
   .then(function(response) {
     return response.json();
   })
-  .then( myJson => {
+  .then(myJson => {
     // console.log(myJson.data.DAILY.dataByMember.players);
     const players = myJson.data.DAILY.dataByMember.players;
     const date = myJson.data.DAILY.dates;
     // let {players: players, dates: date } =  myJson.data.DAILY.dataByMember;
-    let {john: j ,larry:l} = players;
-    
-    // console.log(date);
-    
-   let data = j.points.map((john,i) => {
-      return  {
-        day: date[i],
-        scoreJ: john,
-        scoreL: l.points[i]
+    let { john: j, larry: l } = players;
+
+    let data = j.points.map((john, i) => {
+      const totalPoints = john + l.points[i] || 0;
+      percJohn = percentage(john, totalPoints);
+      percLarry = percentage(l.points[i], totalPoints);
+      
+      return {
+        day: formatDate(date[i]),
+        scoreJ: percJohn,
+        scoreL: percLarry
       };
     });
 
-    console.log(data);
-    
-    
+    displayData(data.filter(i => i.day > 0));
   });
 
 // const chart = document.querySelector('dl')
@@ -33,196 +33,60 @@ fetch("http://cdn.55labs.com/demo/api.json")
 //     element.style.setProperty('--value', '10');
 // });
 
+// displayData(data);
+// animate();
 
-{
-  //list of skills
-  var data = [
-    {
-      name: "SQL",
-      work: 90,
-      personal: 10
-    },
-    {
-      name: "PHP",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "jS",
-      work: 30,
-      personal: 70
-    },
-    {
-      name: "jQuery",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "WordPress",
-      work: 20,
-      personal: 80
-    },
-    {
-      name: "SQL",
-      work: 90,
-      personal: 10
-    },
-    {
-      name: "PHP",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "jS",
-      work: 30,
-      personal: 70
-    },
-    {
-      name: "jQuery",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "WordPress",
-      work: 20,
-      personal: 80
-    },
-    {
-      name: "SQL",
-      work: 90,
-      personal: 10
-    },
-    {
-      name: "PHP",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "jS",
-      work: 30,
-      personal: 70
-    },
-    {
-      name: "jQuery",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "WordPress",
-      work: 20,
-      personal: 80
-    },
-    {
-      name: "SQL",
-      work: 90,
-      personal: 10
-    },
-    {
-      name: "PHP",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "jS",
-      work: 30,
-      personal: 70
-    },
-    {
-      name: "jQuery",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "WordPress",
-      work: 20,
-      personal: 80
-    },
-    {
-      name: "SQL",
-      work: 90,
-      personal: 10
-    },
-    {
-      name: "PHP",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "jS",
-      work: 30,
-      personal: 70
-    },
-    {
-      name: "jQuery",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "WordPress",
-      work: 20,
-      personal: 80
-    },
-    {
-      name: "SQL",
-      work: 90,
-      personal: 10
-    },
-    {
-      name: "PHP",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "jS",
-      work: 30,
-      personal: 70
-    },
-    {
-      name: "jQuery",
-      work: 40,
-      personal: 60
-    },
-    {
-      name: "WordPress",
-      work: 20,
-      personal: 80
-    }
-  ];
+//Functions
+//display data
+function displayData(inputData) {
+  const bar = document.querySelector(".bars");
+  const skill = document.querySelector(".skills");
 
-  displayData(data);
-  // animate();
-
-  //Functions
-  //display data
-  function displayData(inputSkills) {
-    const bar = document.querySelector(".bars");
-    const skill = document.querySelector(".skills");
-
-    const markUpSkills = `${inputSkills
-      .map(
-        (v, i) => `<li style="--start: ${i};"> <span> ${v.name} </li> </span>`
-      )
-      .join("")}
+  const markUpSkills = `${inputData
+    .map((v, i) => `<li style="--start: ${i};"> <span> ${v.day} </li> </span>`)
+    .join("")}
         `;
-    const markUpBar = `${inputSkills
-      .map(
-        (v, i) =>
-          `<li  style="--start: ${i};">
-        <div class='bar'  style="--barwidth: ${v.personal}%;" >
-            ${v.personal}
+  const markUpBar = `${inputData
+    .map(
+      (v, i) =>
+        `<li  style="--start: ${i};">
+        <div class='bar'  style="--barwidth: ${v.scoreJ}%;" >
+            ${v.scoreJ}
         </div>
-        <div class='bar'  style="--barwidth: ${v.work}%;" >
-            ${v.work}
+        <div class='bar'  style="--barwidth: ${v.scoreL}%;" >
+            ${v.scoreL}
         </div>
     </li>`
-      )
-      .join("")}`;
+    )
+    .join("")}`;
 
-    skill.innerHTML = markUpSkills;
-    bar.insertAdjacentHTML("beforeend", markUpBar);
+  skill.innerHTML = markUpSkills;
+  bar.insertAdjacentHTML("beforeend", markUpBar);
+}
+function formatDate(inputDate) {
+  if (inputDate) {
+    const reversed = inputDate.split("").join("");
+    const addYear = insert(reversed, 4, '/');
+    const addMonth = insert(addYear, 7, '/').join("");
+     
+    console.log(addMonth);
+    
+    return addMonth;
+  } else {
+    return 0;
   }
 }
 
+const insert = (arr, index, newItem) => [
+  ...arr.slice(0, index),
+  newItem,
+  ...arr.slice(index)
+]
+
 function percentage(a, b) {
-  return Math.floor((a / b) * 100);
-};
+  if (a && b) {
+    return Math.floor((a / b) * 100);
+  } else {
+    return 0;
+  }
+}
